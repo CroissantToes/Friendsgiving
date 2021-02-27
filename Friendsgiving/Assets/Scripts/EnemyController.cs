53fe74190a50ab67727109c5eb870672dffd6499
CroissantToes <CroissantToes@users.noreply.github.com>
2021-02-27 16:50:32 -0500

    Enemy movement

---
+++ Friendsgiving/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float speed;
+    public float stopDistance;
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, target.position) > stopDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        
+    }
+
+}
