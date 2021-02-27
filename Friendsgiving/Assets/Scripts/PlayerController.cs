1952e5ecfcd68af966641528ce8da23b7ee5bdea
CroissantToes <CroissantToes@users.noreply.github.com>
2021-02-27 16:18:06 -0500

    Movement

--- Friendsgiving/Assets/Scripts/PlayerController.cs
+++ Friendsgiving/Assets/Scripts/PlayerController.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+
+    public float runSpeed;
+
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
     }
 
-    //Called before any physics calculations
     private void FixedUpdate()
     {
-        
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
 }
