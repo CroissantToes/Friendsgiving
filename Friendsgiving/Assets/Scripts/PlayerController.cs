6dca41989b9cbb0aa7f8e2092637a229b6f04b8a
CroissantToes <CroissantToes@users.noreply.github.com>
2021-02-27 20:03:26 -0500

    level switching and health

--- Friendsgiving/Assets/Scripts/PlayerController.cs
+++ Friendsgiving/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,18 +10,35 @@ public class PlayerController : MonoBehaviour
     float horizontal;
     float vertical;
     public int health;
-
     public float runSpeed;
+    public Vector2 level2;
+    public Vector3 level3;
+    public Text lifeText;
 
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        SetHealthText();
     }
 
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+
+        if(health == 4)
+        {
+            transform.position = level2;
+            health = 1;
+        }
+
+        if(health == 4)
+        {
+            transform.position = level3;
+            health = 1;
+        }
+
+        SetHealthText();
     }
 
     private void FixedUpdate()
@@ -28,14 +46,20 @@ public class PlayerController : MonoBehaviour
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("Friend"))
         {
+            Debug.Log("hit");
             other.gameObject.SetActive(false);
             health++;
         }
       
 
+    }
+
+    void SetHealthText()
+    {
+        lifeText.text = "Health: " + health.ToString();
     }
 }
