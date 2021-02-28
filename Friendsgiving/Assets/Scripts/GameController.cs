8941e27ca8170b5a5cbd3cb09c6645b903bedd41
CroissantToes <CroissantToes@users.noreply.github.com>
2021-02-27 23:08:55 -0500

    text at start

--- Friendsgiving/Assets/Scripts/GameController.cs
+++ Friendsgiving/Assets/Scripts/GameController.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject screen;
+    public Text lines;
+    public int lineCounter = 0;
+
+    public GameObject player;
+    public GameObject enemies;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lines.text = "Birdman";
     }
 
     // Update is called once per frame
@@ -19,6 +27,31 @@ public class GameController : MonoBehaviour
         {
             Debug.Log("escape key pressed!");
             Application.Quit();
+        }
+    }
+
+    public void NextLine()
+    {
+        if(lineCounter == 0)
+        {
+            lines.text = "You used to feed the birds with your wife, but that was a long time ago.";
+            lineCounter++;
+        }
+        else if(lineCounter == 1)
+        {
+            lines.text = "She's gone now. The only time you still feel close to her is when you go see the birds.";
+            lineCounter++;
+        }
+        else if(lineCounter == 2)
+        {
+            lines.text = "Reclaim your happiness by collecting bird friends.";
+            lineCounter++;
+        }
+        else
+        {
+            screen.gameObject.SetActive(false);
+            player.gameObject.SetActive(true);
+            enemies.gameObject.SetActive(true);
         }
     }
 }
